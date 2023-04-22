b83bdfd6060d96422cbca0cb3a2c9b1344379241
sandorkiss@stud.uni-obuda.hu <sandorkiss@stud.uni-obuda.hu>
2023-04-22 16:52:55 +0200

    Same as 2 seconds ago

--- C6XSDH_HW2/Trie.cpp
+++ C6XSDH_HW2/Trie.cpp
@@ -5,3 +5,26 @@ Trie::Trie()
 {
 	mRoot = std::shared_ptr<TrieNode>();
 }
+
+void Trie::insert(const std::string& word)
+{
+	std::shared_ptr<TrieNode> node = mRoot;
+	for (char c : word) 
+	{
+		//if(node) 
+	}
+}
+
+bool Trie::search(const std::string& word)
+{
+	return false;
+}
+
+void Trie::readFromFile(const std::string& path)
+{
+}
+
+void Trie::isCorrect(const std::string& word)
+{
+}
+
