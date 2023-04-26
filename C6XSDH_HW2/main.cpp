376b5195616bb50f846106cdcaa0c3b97809cbaa
sandorkiss@stud.uni-obuda.hu <sandorkiss@stud.uni-obuda.hu>
2023-04-26 11:58:09 +0200

    Testing the trie in main

--- C6XSDH_HW2/main.cpp
+++ C6XSDH_HW2/main.cpp
@@ -1,10 +1,21 @@
 #include "TrieNode.h"
 #include "Trie.h"
-
+#include <iostream>
 
 int main() 
 {
-	
+	Trie dictionary;
+	dictionary.insert("alma");
+	std::cout << dictionary.search("alma") << std::endl;		//1
+	std::cout << dictionary.search("korte") << std::endl;		//0
+	dictionary.insert("bicikli");
+	dictionary.insert("korte");
+	std::cout << dictionary.search("korte") << std::endl;		//1
+	std::cout << dictionary.search("auto") << std::endl;		//0
+	std::cout << dictionary.search("bicikli") << std::endl;		//1
+	std::cout << dictionary.startsWith("al") << std::endl;		//1
+	std::cout << dictionary.startsWith("ku") << std::endl;		//0
+
 
 	return 0;
 }
